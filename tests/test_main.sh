3a15ad8125d3d8beca4948163758b91cb732cba7
Takuma Tsubo <ttsubo@c4r2s12.42tokyo.jp>
2026-05-24 19:04:58 +0900

    listenのテストを追加

--- tests/test_main.sh
+++ tests/test_main.sh
@@ -14,6 +14,28 @@ check "正しい引数でServer listening" \
 	"Server listening on port 6667" \
 	"$(cat /tmp/ircserv_out.txt)"
 
+(echo > /dev/tcp/127.0.0.1/6667) 2>/dev/null
+check "ポート6667でlistenしている" "0" "$?"
+
+# TODO　クライアント接続が発生するテストの作り方を整理しておく
+# nc 127.0.0.1 6667 < /dev/null
+# sleep 0.2
+# check "クライアント接続のログ" \
+#     "1" \
+#     "$(grep -c 'New client connected' /tmp/ircserv_out.txt)"
+
+# echo "hello" | nc 127.0.0.1 6667
+# sleep 0.2
+# check "データ受信のログ" \
+#     "1" \
+#     "$(grep -c 'Recieved' /tmp/ircserv_out.txt)"
+
+# # 切断ログの確認
+# check "クライアント切断のログ" \
+#     "1" \
+#     "$(grep -c 'Client disconnected' /tmp/ircserv_out.txt)"
+
+
 kill $SERVER_PID 2>/dev/null
 wait $SERVER_PID 2>/dev/null
 rm -f /tmp/ircserv_out.txt
