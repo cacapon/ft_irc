c10d498cebbd822b80b016045ed9b0daadcaf612
Cacapon Project <cacapon.project@gmail.com>
2026-05-29 21:24:51 +0900

    クライアントクラス作成

---
+++ incs/Client.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+
+class Client
+{
+  private:
+	int _fd;
+	std::string _nick;
+	std::string _user;
+	std::string _recvBuf;
+	bool _passOk;
+
+  public:
+	Client();
+	Client(int);
+	Client(const Client &);
+	Client &operator=(const Client &);
+	~Client();
+
+	int getFd() const;
+	std::string getNick() const;
+	std::string getUser() const;
+	bool isPassOk() const;
+	std::string getRecvBuf() const;
+
+	void setNick(const std::string &nick);
+	void setUser(const std::string &user);
+	void setPassOk(bool ok);
+
+	// RecvBufは+=で追記するため、Setterの代わりに以下を使う
+	void appendRecvBuf(const std::string &data);
+	void eraseRecvBuf(size_t len);
+};
---
+++ srcs/Client.cpp
@@ -0,0 +1,72 @@
+#include "Client.hpp"
+
+Client::Client(): _fd(-1), _passOk(false)
+{
+}
+Client::Client(int fd) : _fd(fd), _passOk(false)
+{
+}
+Client::Client(const Client &src) : _fd(src.getFd()), _nick(src.getNick()),
+	_user(src.getUser()), _recvBuf(src.getRecvBuf()), _passOk(src.isPassOk())
+{
+}
+Client &Client::operator=(const Client &other)
+{
+	if (this != &other)
+	{
+		_fd = other.getFd();
+		_nick = other.getNick();
+		_user = other.getUser();
+		_recvBuf = other.getRecvBuf();
+		_passOk = other.isPassOk();
+	}
+	return (*this);
+}
+Client::~Client()
+{
+}
+
+int Client::getFd() const
+{
+	return (_fd);
+}
+std::string Client::getNick() const
+{
+	return _nick;
+}
+std::string Client::getUser() const
+{
+	return _user;
+}
+bool Client::isPassOk() const
+{
+	return _passOk;
+}
+std::string Client::getRecvBuf() const
+{
+	return _recvBuf;
+}
+
+void Client::setNick(const std::string &nick)
+{
+	_nick = nick;
+}
+void Client::setUser(const std::string &user)
+{
+	_user = user;
+}
+void Client::setPassOk(bool ok)
+{
+	_passOk = ok;
+}
+
+void Client::appendRecvBuf(const std::string &data)
+{
+	_recvBuf += data;
+}
+
+// \r\nの分はメソッド内で消す
+void Client::eraseRecvBuf(size_t pos)
+{
+    _recvBuf.erase(0, pos + 2);
+}
